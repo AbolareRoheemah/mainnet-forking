9260a437d779d9ab655fd47991fb626d06683281
Abolare Roheemah Ayomide <roheemah@Abolares-MacBook-Air.local>
2024-09-15 20:38:52 +0100

    finally able to add eth liquidity

--- scripts/mainnet-forking.ts
+++ scripts/mainnet-forking.ts
@@ -11,13 +11,16 @@ async function main() {
     await helpers.impersonateAccount(TOKEN_HOLDER);
     const impersonatedSigner = await ethers.getSigner(TOKEN_HOLDER);
 
-    const amountADesired = ethers.parseUnits("200", 6);
+    const amountADesired = ethers.parseUnits("2000", 6);
+    const liquidity = ethers.parseUnits("200", 18);
     const amountBDesired = ethers.parseUnits("200", 18);
-    const amountAMin = ethers.parseUnits("100", 6);
+    const amountAMin = ethers.parseUnits("10", 6);
     const amountBMin = ethers.parseUnits("100", 18);
+    const amountEthMin = ethers.parseEther("0.1");
 
     const USDC_Contract = await ethers.getContractAt("IERC20", USDC, impersonatedSigner);
     const DAI_Contract = await ethers.getContractAt("IERC20", DAI, impersonatedSigner);
+    const amountETHDesired = ethers.parseEther("1");
     
     const ROUTER = await ethers.getContractAt("IUniswapV2Router", ROUTER_ADDRESS, impersonatedSigner);
 
@@ -26,34 +29,59 @@ async function main() {
 
     const usdcBal = await USDC_Contract.balanceOf(impersonatedSigner.address);
     const daiBal = await DAI_Contract.balanceOf(impersonatedSigner.address);
+    const ethBal = await ethers.provider.getBalance(impersonatedSigner.address);
     const deadline = Math.floor(Date.now() / 1000) + (60 * 10);
 
-    console.log("usdc balance before swap", Number(usdcBal));
-    console.log("dai balance before swap", Number(daiBal));
+    console.log("usdc balance before swap/liquidity tx", Number(usdcBal));
+    console.log("dai balance before swap/liquidity tx", Number(daiBal));
+    console.log("eth balance before swap/liquidity tx", Number(ethBal));
 
-    await ROUTER.addLiquidity(
+    // await ROUTER.addLiquidity(
+    //     USDC_Contract,
+    //     DAI_Contract,
+    //     amountADesired,
+    //     amountBDesired,
+    //     amountAMin,
+    //     amountBMin,
+    //     impersonatedSigner.address,
+    //     deadline
+    // );
+
+    // await ROUTER.removeLiquidity(
+    //     USDC_Contract,
+    //     DAI_Contract,
+    //     liquidity,
+    //     amountAMin,
+    //     amountBMin,
+    //     impersonatedSigner.address,
+    //     deadline
+    // );
+
+    await ROUTER.addLiquidityETH(
         USDC_Contract,
-        DAI_Contract,
         amountADesired,
-        amountBDesired,
         amountAMin,
-        amountBMin,
+        amountEthMin,
         impersonatedSigner.address,
-        deadline
-    );
+        deadline,
+        { value: amountETHDesired }
+    )
 
     const usdcBalAfter = await USDC_Contract.balanceOf(impersonatedSigner.address);
     const daiBalAfter = await DAI_Contract.balanceOf(impersonatedSigner.address);
+    const ethBalAfter = await ethers.provider.getBalance(impersonatedSigner.address);
 
     console.log("=========================================================");
 
-    console.log("usdc balance after swap", Number(usdcBalAfter));
-    console.log("dai balance after swap", Number(daiBalAfter));
+    console.log("usdc balance after swap/liquidity tx", Number(usdcBalAfter));
+    console.log("eth balance after swap/liquidity tx", Number(ethBalAfter));
+    console.log("dai balance after swap/liquidity tx", Number(daiBalAfter));
 
     console.log("=========================================================");
 
-    console.log("usdc balance after swap",Number(usdcBal) - Number(usdcBalAfter));
-    console.log("dai balance after swap", Number(daiBal) - Number(daiBalAfter));
+    console.log("usdc swapped or liquidity added/removed",Number(usdcBal) - Number(usdcBalAfter));
+    console.log("eth swapped or liquidity added/removed", Number(ethBal) - Number(ethBalAfter));
+    console.log("dai swapped or liquidity added/removed", Number(daiBal) - Number(daiBalAfter));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
