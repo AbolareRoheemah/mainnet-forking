5fc5078ce1d8cd65cf7375a9510e1275740627c2
Abolare Roheemah <roheemahabo@gmail.com>
2024-10-01 18:37:20 +0100

    updates to interactions

--- scripts/mainnet-forking.ts
+++ scripts/mainnet-forking.ts
@@ -5,6 +5,7 @@ async function main() {
     const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
     const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
     const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const pair = "0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5"
 
     const TOKEN_HOLDER = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621";
 
@@ -12,7 +13,7 @@ async function main() {
     const impersonatedSigner = await ethers.getSigner(TOKEN_HOLDER);
 
     const amountADesired = ethers.parseUnits("2000", 6);
-    const liquidity = ethers.parseUnits("200", 18);
+    const liquidity = ethers.parseUnits("1", 18);
     const amountBDesired = ethers.parseUnits("200", 18);
     const amountAMin = ethers.parseUnits("10", 6);
     const amountBMin = ethers.parseUnits("100", 18);
@@ -20,12 +21,14 @@ async function main() {
 
     const USDC_Contract = await ethers.getContractAt("IERC20", USDC, impersonatedSigner);
     const DAI_Contract = await ethers.getContractAt("IERC20", DAI, impersonatedSigner);
+    const pair_contract = await ethers.getContractAt("IERC20", pair, impersonatedSigner);
     const amountETHDesired = ethers.parseEther("1");
     
     const ROUTER = await ethers.getContractAt("IUniswapV2Router", ROUTER_ADDRESS, impersonatedSigner);
 
     await USDC_Contract.approve(ROUTER, amountADesired);
     await DAI_Contract.approve(ROUTER, amountBDesired);
+    await pair_contract.approve(ROUTER, liquidity);
 
     const usdcBal = await USDC_Contract.balanceOf(impersonatedSigner.address);
     const daiBal = await DAI_Contract.balanceOf(impersonatedSigner.address);
@@ -47,25 +50,25 @@ async function main() {
     //     deadline
     // );
 
-    // await ROUTER.removeLiquidity(
-    //     USDC_Contract,
-    //     DAI_Contract,
-    //     liquidity,
-    //     amountAMin,
-    //     amountBMin,
-    //     impersonatedSigner.address,
-    //     deadline
-    // );
-
-    await ROUTER.addLiquidityETH(
+    await ROUTER.removeLiquidity(
         USDC_Contract,
-        amountADesired,
+        DAI_Contract,
+        liquidity,
         amountAMin,
-        amountEthMin,
+        amountBMin,
         impersonatedSigner.address,
-        deadline,
-        { value: amountETHDesired }
-    )
+        deadline
+    );
+
+    // await ROUTER.addLiquidityETH(
+    //     USDC_Contract,
+    //     amountADesired,
+    //     amountAMin,
+    //     amountEthMin,
+    //     impersonatedSigner.address,
+    //     deadline,
+    //     { value: amountETHDesired }
+    // )
 
     const usdcBalAfter = await USDC_Contract.balanceOf(impersonatedSigner.address);
     const daiBalAfter = await DAI_Contract.balanceOf(impersonatedSigner.address);
